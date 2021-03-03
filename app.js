3c8b0efeff9f4a5abf2fa2eb46aa1c75a0c77d14
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-03-03 17:26:12 -0300

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log('oi');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
+
+
+function loop() {
+    contexto.drawImage(
+        sprites,
+        0, 0,
+        33, 24,
+        10, 50,
+        33, 24,
+    );
+
+    requestAnimationFrame(loop);
+}
+
+loop();
